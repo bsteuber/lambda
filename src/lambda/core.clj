c0271f925e4969ad7167e377c34e83939083ef34
Benjamin Teuber <bsteuber@gmail.com>
2016-06-02 16:05:55 +0100

    Add with-error-pprint macro

--- src/lambda/core.clj
+++ src/lambda/core.clj
@@ -11,3 +11,13 @@
         res (e/eval term)
         formatted-res (t/format res)]
     [type formatted-res]))
+
+(defmacro with-error-pprint [& body]
+  `(try
+     ~@body
+     (catch Exception e#
+       (if-let [dat# (ex-data e#)]
+         (do (println "ERROR:")
+             (println " " (.getMessage e#))
+             (println " " dat#))
+         (throw e#)))))
